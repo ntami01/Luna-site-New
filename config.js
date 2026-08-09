de0a89f8caf8c2ea7cd600f860a4d2b737597e17
ntami01 <ami.nt1101@gmail.com>
2026-08-10 01:08:13 +0900

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-window.LUNA_CONFIG = Object.freeze({
-  sheetCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTpEuqUi4WT4V0EhZ7v7klopaRgpVzMmmhMDqIC4njUU322i-tWU9kNW5jr_m9PQi18YzqiZH6LFiSK/pub?gid=1514190748&single=true&output=csv"
-});
